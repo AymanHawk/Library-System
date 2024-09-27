2d359fe39e69b8f6b300ba28619d575a91138b0c
Katie Andor <superkga5678@gmail.com>
2024-09-27 15:05:19 -0400

    updated home page to match figma, didn't include the top part of the design because I think that should be put in layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,25 +9,25 @@ import { ImagesSlider } from "../components/aceturnity-ui/ImageSlider";
 import { InfiniteMovingCards } from "../components/aceturnity-ui/messageSlider";
 
 export default function Home() {
-
   return (
-    <div className="flex flex-1">
-      <div
-        className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"></div>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"></div>
-          ))}
-        </div>
+    <div className="bg-[#1E1C1C]">
+      <div className="text-[#DCE75C] text-[4vw] ml-[5%] mt-[5%] mr-[50%] leading-tight">
+        <h1>
+          The Library <br /> Delivery <br /> System
+        </h1>
+      </div>
+      <div className="text-white text-[1.4vw] ml-[5%] mt-[2%] mr-[30%]">
+        <h2>
+          Insert explanation of our service here, Lorem ipsum odor amet,
+          consectetuer adipiscing elit. Euismod felis egestas nulla tellus mus;
+          luctus fermentum. Augue nibh vehicula lacinia amet, varius est porta.
+          Nunc fames montes purus penatibus gravida vehicula cubilia. Aposuere
+          suspendisse gravida curae taciti maecenas. Penatibus taciti morbi
+          accumsan velit senectus sodales tempor.
+        </h2>
+      </div>
+      <div>
+        <button className="text-white text-[1.3vw] ml-[5%] mt-[2%] bg-[#5D68B0] p-2 pr-6 pl-6 rounded-md"> Browse </button>
       </div>
     </div>
   );
