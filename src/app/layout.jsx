8638469857b0328b036b5dfd92461c66ce4db02e
Rishi <rpate290@nyit.edu>
2024-10-02 11:59:39 -0400

    switched from supertokens auth to clerk auth

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,12 @@
 import React from "react";
 import "./globals.css";
 import Navbar from "../components/Navbar.jsx"
-
+import{ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton}
+  from '@clerk/nextjs'
 
 
 
@@ -11,11 +16,19 @@ export default function RootLayout({ children }) {
 
   
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className="bg-background p-5 text-white">
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           <Navbar />
             {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
