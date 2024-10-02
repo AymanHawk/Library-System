1c242d2e09ee2be4ddf84be463c488382cd8e05a
Katie Andor <superkga5678@gmail.com>
2024-10-02 17:45:24 -0400

    Made dashboard page without the navigation bar for now, all link working and mobile responsive

--- src/app/user/dashboard/[id]/page.jsx
+++ src/app/user/dashboard/[id]/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-3 ml-[5%] mr-[5%]">
+      <div>
+        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Orders</h1>
+        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/orders">See All</a></div>
+      </div>
+      <div>
+        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Recommendations</h1>
+        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/recommendation">See All</a></div>
+      </div>
+      <div>
+        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Read Recently</h1>
+        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/list/readBooks">See All</a></div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
