df4a61a5454bb5705cf524b0384a21bfe17b264c
Katie Andor <superkga5678@gmail.com>
2024-10-02 23:56:47 -0400

    added divs and dashboard navbar component, will be mobile responsive when I can figure out how to get the bottom two of the grid center when the screen gets small

--- src/app/user/dashboard/[id]/page.jsx
+++ src/app/user/dashboard/[id]/page.jsx
@@ -1,19 +1,36 @@
+'use client'
 import React from "react";
+import {DashboardNavbar }from "../../../../components/DashboardNavbar";
 
 function Dashboard() {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-3 ml-[5%] mr-[5%]">
-      <div>
-        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Orders</h1>
-        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/orders">See All</a></div>
-      </div>
-      <div>
-        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Recommendations</h1>
-        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/recommendation">See All</a></div>
-      </div>
-      <div>
-        <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">Read Recently</h1>
-        <div className="border border-secondary rounded-[5px] border-2 p-2"><a href="/user/list/readBooks">See All</a></div>
+    <div>
+      <DashboardNavbar />
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-3">
+        <div>
+          <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
+            Orders
+          </h1>
+          <div className="border border-secondary rounded-[5px] border-2 p-2">
+            <a href="/user/orders">See All</a>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
+            Recommendations
+          </h1>
+          <div className="border border-secondary rounded-[5px] border-2 p-2">
+            <a href="/user/recommendation">See All</a>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-primary sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
+            Read Recently
+          </h1>
+          <div className="border border-secondary rounded-[5px] border-2 p-2">
+            <a href="/user/list/readBooks">See All</a>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/DashboardNavbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const DashboardNavbar = () => {
+  return (
+    <div className="text-primary m-[3%] sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl justify-items-center grid gap-1 grid-cols-3 lg-grid-cols-5 xl:grid-cols-5 justify-items-center">
+      <h2>Dashboard</h2>
+      <h2>Statistics</h2>
+      <h2>Orders</h2>
+      <h2>Recommendations</h2>
+      <h2>Books List</h2>
+    </div>
+  );
+};
