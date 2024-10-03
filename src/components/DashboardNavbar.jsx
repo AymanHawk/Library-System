2494434595af15903e3c5282180c86554011f173
Katie Andor <superkga5678@gmail.com>
2024-10-03 00:00:24 -0400

    changed some margins

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const DashboardNavbar = () => {
   return (
-    <div className="text-primary m-[3%] sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl justify-items-center grid gap-1 grid-cols-3 lg-grid-cols-5 xl:grid-cols-5 justify-items-center">
+    <div className="text-primary mt-[3%] mb-[3%] mr-[6%] ml-[6%] sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl justify-items-center grid gap-1 grid-cols-3 lg-grid-cols-5 xl:grid-cols-5 justify-items-center">
       <h2>Dashboard</h2>
       <h2>Statistics</h2>
       <h2>Orders</h2>
