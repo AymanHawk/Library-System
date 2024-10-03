1a86032c88324c4da3a96e7c7c586b966daf5d41
Bhavpreet Singh <bhavpreet666singh@gmail.com>
2024-10-03 17:57:19 -0400

    staging for the merge to testing

--- src/app/user/recommendation/[id]/page.jsx
+++ src/app/user/recommendation/[id]/page.jsx
@@ -6,23 +6,23 @@ function Recomendations() {
     <div>
       <DashboardNavbar />
       <div>
-        <div className="border border-secondary rounded-[5px] border-2 p-3 mb-[5%]">
+        <div className=" border-secondary rounded-[5px] border-2 p-3 mb-[5%]">
           <div className="flex justify-between items-center">
             <h1 className="text-white sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
               Based on Survey
             </h1>
-            <a href="" className="text-1xl">
+            <a href="#" className="text-1xl">
               See All
             </a>
           </div>
           {/* Books go here */}
         </div>
-        <div className="border border-secondary rounded-[5px] border-2 p-3">
+        <div className=" border-secondary rounded-[5px] border-2 p-3">
           <div className="flex justify-between items-center">
             <h1 className="text-white sm:text-2xl md:text-2xl lg:text-4xl xl:text-4xl">
               Based on Recent Reads
             </h1>
-            <a href="" className="text-1xl">
+            <a href="#" className="text-1xl">
               See All
             </a>
             {/* Books go here */}
