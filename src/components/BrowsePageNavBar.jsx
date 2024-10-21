45e132f1fdffbd5004f24b9789a8e8d23872ce05
Katie Andor <superkga5678@gmail.com>
2024-10-21 12:45:06 -0400

    added component for navbar and set up the components that display the content 10/21 12:44pm

---
+++ src/components/BrowsePageNavBar.jsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import Home from "./Home.jsx";
+import Fiction from "./Fiction.jsx";
+import Popular from "./Popular.jsx";
+import InYourArea from "./InYourArea.jsx";
+
+export default function BrowserNavbar() {
+  const [activeTab, setActiveTab] = useState("home");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <div className="text-primary my-[3%] mx-[6%] text-center text-sm sm:text-base md:text-xl norm:text-2xl lg:text-3xl sm:flex justify-start hidden">
+        <button className="mr-6 p-2 hover:bg-primary w-full hover:text-black" onClick={() => handleTabChange("home")}>Home</button>
+        <button className="mr-6 p-2 hover:bg-primary w-full hover:text-black" onClick={() => handleTabChange("fiction")}>Fiction</button>
+        <button className="mr-6 p-2 hover:bg-primary w-full hover:text-black" onClick={() => handleTabChange("popular")}>Popular</button>
+        <button className="mr-6 p-2 hover:bg-primary w-full hover:text-black" onClick={() => handleTabChange("inyourarea")}>
+          In Your Area
+        </button>
+        <a href="" className="mr-6 p-2 hover:bg-primary w-full hover:text-black">
+        <button>
+          Custom
+        </button>
+        </a>
+      </div>
+
+      <div className="tab-content">
+        {activeTab === "home" && (
+          <div>
+            <Home />
+          </div>
+        )}
+
+        {activeTab === "fiction" && (
+          <div>
+            <Fiction />
+          </div>
+        )}
+
+        {activeTab === "popular" && (
+          <div>
+            <Popular />
+          </div>
+        )}
+
+        {activeTab === "inyourarea" && (
+          <div>
+            <InYourArea />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Fiction.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Fiction() {
+  return (
+    <div>
+      <h1 className="ml-[10%] mt-[5%]">Fiction Component</h1>
+    </div>
+  );
+}
