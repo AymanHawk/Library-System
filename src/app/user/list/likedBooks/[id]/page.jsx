762f9d1bea3a56677005d8e43b5eaa11c730a70a
Katie Andor <superkga5678@gmail.com>
2024-10-27 19:14:23 -0400

    mobile responsive

--- src/app/user/list/likedBooks/[id]/page.jsx
+++ src/app/user/list/likedBooks/[id]/page.jsx
@@ -39,28 +39,37 @@ function likedBooks() {
   return (
     <div>
       <UserNavbar />
-      <div className="ml-auto mr-auto">
-        <h1 className="mb-[10px] text-primary text-[43px]">Liked Books</h1>
-        <div className="flex flex-row flex-wrap justify-start items-center gap-6">
-          {list.map((book) => (
-            <div key={book.id} className="flex flex-col ml-auto mr-auto w-[400px] h-[550px] xl:w-[350px] xl:h-[500px] lg:w-[300px] lg:h-[630px] md:w-[280px] md:h-[630px] sm:w-[200px] sm:h-[580px]">
-              <Image
-                src={book.imgUrl}
-                alt={book.id}
-                width={50}
-                height={60}
-                className="xl:w-[200px] xl:h-[300px] lg:w-[250px] lg:h-[350px] md:w-[250px] md:h-[350px] sm:w-[200px] w-[200px]"
-              />
-              <h2 className="text-primary text-[32px]">{book.title.length > 20 ? `${book.title.slice(0, 20)}...` : book.title}</h2>
-              <div className="h-full"></div>
-              <h3 className="text-[23px]">{book.author}</h3>
-              <h3 className="text-[23px] mb-[30px]">{book.genre}</h3>
-              <div className="h-full"></div>
-              <button className="text-center text-[20px] bg-secondary w-full p-2">
-                Remove
-              </button>
-            </div>
-          ))}
+      <div className="">
+        <h1 className="mb-[10px] lg:ml-[100px] text-primary text-[43px]">Liked Books</h1>
+        <div className="lg:ml-[100px] flex flex-row flex-wrap justify-start gap-6">
+          <div className="flex flex-wrap gap-6 justify-center lg:justify-start">
+            {list.map((book) => (
+              <div
+                key={book.id}
+                className="flex flex-col lg:mb-[25px] w-[400px] h-[550px] xl:w-[270px] xl:h-[600px] lg:w-[300px] lg:h-[630px] md:w-[280px] md:h-[630px] sm:w-[200px] sm:h-[580px]"
+              >
+                <Image
+                  src={book.imgUrl}
+                  alt={book.id}
+                  width={50}
+                  height={60}
+                  className="xl:w-[250px] xl:h-[350px] lg:w-[250px] lg:h-[350px] md:w-[260px] md:h-[350px] w-[200px] h-[300px]"
+                />
+                <h2 className="text-primary text-[32px]">
+                  {book.title.length > 20
+                    ? `${book.title.slice(0, 20)}...`
+                    : book.title}
+                </h2>
+                <div className="h-full"></div>
+                <h3 className="text-[23px]">{book.author}</h3>
+                <h3 className="text-[23px] mb-[30px]">{book.genre}</h3>
+                <div className="h-full"></div>
+                <button className="text-center text-[20px] bg-secondary w-full p-2">
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
