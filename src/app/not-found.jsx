20a66c3b3902d11e4f77e5463211aa1b835421b1
Rishi <rpate290@nyit.edu>
2024-11-14 21:20:57 -0500

    added error page and not-found page using the design katie sent

---
+++ src/app/not-found.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+export default function Error() {
+    return (
+      <div className=" text-primary 2xl:w-[1300px] xl:w-[1200px] lg:w-[1000px] norm:w-[750px] md:w-[600px] sm:w-[450px] w-[340px] xs:w-[275px] mx-auto text-center py-8 border border-primary">
+
+            <h1 className="text-6xl font-bold font-sans">Oops!</h1>
+              <h3 className="text-4xl font-sans">Looks like we ran into an <br />error!</h3>
+        </div>
+      );
+  }
+  
