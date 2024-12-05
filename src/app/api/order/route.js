35a2bec058b57f94f73df206caad2b3fabb6f0df
fangedShadow <bhavpreet666singh.com>
2024-12-05 07:21:46 -0500

    order page done, order return funtionality ready, only user preference page and dashboard left

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,15 +1,13 @@
-import clientPromise from "../../../../../lib/dbConnection/mongoDB";
+import clientPromise from "../../../lib/dbConnection/mongoDB";
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
-
 export async function GET(req) {
     try {
-        const libId = req.headers.get('libId');
-        const limit = parseInt(req.headers.get('limit')) || 15;
-        const page = parseInt(req.headers.get('page')) || 1;
-        console.log(libId)
-        if (!libId) {
+
+        const orderId = req.headers.get('orderId');
+
+        if (!orderId) {
             return new Response(JSON.stringify({ success: false, message: 'Missing data in request' }), {
                 status: 400,
                 headers: { 'Content-Type': 'application/json' },
@@ -18,15 +16,12 @@ export async function GET(req) {
 
         const client = await clientPromise;
         const db = client.db('lib');
-        const skip = (page - 1) * limit;
 
-        const library = await db.collection('libraries').findOne({ authId: libId });
-        const orders = await db.collection('orders').aggregate(
+        const order = await db.collection('orders').aggregate(
             [
                 {
                     $match: {
-                        libId: new ObjectId(library._id),
-                        orderStatus: 'return'
+                        _id: new ObjectId(orderId),
                     }
                 },
                 {
@@ -73,21 +68,33 @@ export async function GET(req) {
                                 title: "$bookDetails.title",
                                 imgSrc: "$bookDetails.imgUrl",
                                 author: "$bookDetails.author",
-                                isbn: "$bookDetails.isbn"
+                                isbn: "$bookDetails.isbn",
+                                genre: "$bookDetails.genre"
                             }
                         }
                     }
                 },
                 {
                     $lookup: {
-                        from: "users",
-                        localField: "userId",
+                        from: "libraries",
+                        localField: "libId",
                         foreignField: "_id",
-                        as: "UserDetails"
+                        as: "LibDetails"
                     }
                 },
                 {
-                    $unwind: "$UserDetails"
+                    $unwind: "$LibDetails"
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "userId",
+                        foreignField: "_id",
+                        as: "userDetails"
+                    }
+                },
+                {
+                    $unwind: "$userDetails"
                 },
                 {
                     $project: {
@@ -100,18 +107,51 @@ export async function GET(req) {
                         fullfilledAt: "$fullfilledAt",
                         orderStatus: "$orderStatus",
                         books: "$books",
-                        userName: "$UserDetails.name"
+                        libName: "$LibDetails.name",
+                        userName: "$userDetails.name",
+                        userImg: "$userDetails.profileImg"
                     }
                 },
-                { $skip: skip },
-                { $limit: limit }
             ]
         ).toArray();
 
-        const totalCount = orders.length;
+        return NextResponse.json({ order:order[0] }, { status: 200 });
 
-        return NextResponse.json({ orders, totalCount }, { status: 200 });
 
+    } catch (error) {
+        return new Response(JSON.stringify({ success: false, error: error.message }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
+
+export async function POST(req) {
+    try {
+        const body = await req.json();
+        const { orderId } = body
+        const client = await clientPromise;
+        const db = client.db('lib');
+
+        const order = await db.collection('orders').findOne({ _id: new ObjectId(orderId) });
+
+        console.log(order);
+        const result = await db.collection('orders').updateOne(
+            { _id: new ObjectId(orderId) },
+            {
+                $set: {
+                    orderStatus: 'return',
+                    fullfilledAt: new Date()
+                }
+            }
+        );
+
+        const updatedOrder = await db.collection('orders').findOne({ _id: new ObjectId(orderId) });
+
+        return new Response(JSON.stringify({ success: true, order: updatedOrder }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
     } catch (error) {
         return new Response(JSON.stringify({ success: false, error: error.message }), {
             status: 500,
