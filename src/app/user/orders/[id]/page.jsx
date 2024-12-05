d513c2c32ff61b016c0b4f4c9ac9aaf6d2edf838
Katie Andor <superkga5678@gmail.com>
2024-12-05 15:28:38 -0500

    styled customer order page, not responsive

--- src/app/user/orders/[id]/page.jsx
+++ src/app/user/orders/[id]/page.jsx
@@ -1,13 +1,12 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { usePathname } from 'next/navigation';
+"use client";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import UserNavbar from "../../../../components/UserNavbar";
-import Pagination from '../../../browse/books/search/results/Pagination.jsx';
-
+import Pagination from "../../../browse/books/search/results/Pagination.jsx";
 
 function orderId() {
   const pathname = usePathname();
-  const id = pathname.split('/').pop();
+  const id = pathname.split("/").pop();
   const [orders, setOrders] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const limit = 15;
@@ -15,14 +14,14 @@ function orderId() {
 
   const getOrders = async () => {
     try {
-      const res = await fetch('/api/user/orders', {
-        method: 'GET',
+      const res = await fetch("/api/user/orders", {
+        method: "GET",
         headers: {
           userId: id,
           limit: limit,
-          page: currentPage
-        }
-      })
+          page: currentPage,
+        },
+      });
 
       if (!res.ok) {
         throw new Error(`Error fetching orders`);
@@ -37,69 +36,63 @@ function orderId() {
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getOrders()
-  }, [])
+    getOrders();
+  }, []);
 
   return (
     <div>
       <UserNavbar userId={id} userPath={pathname} />
-      {(orders && orders.length > 0) ? (
-        orders.map(order => (
-          <div key={order._id} className='border-secondary border-[1px] py-2'>
-            <h3>Order ID: {order._id}</h3>
-            <div>
-              {order.books.map(book => (
-                <div key={book._id} className='flex flex-col items-center border-secondary border-[1px]'>
-                  <div>{book.title}</div>
 
+      <div className="w-[80%] mx-auto">
+        <h1 className="text-primary text-3xl text-left lg:text-4xl mb-5">
+          Orders
+        </h1>
+        <div className="border-[1px] border-secondary border-solid rounded-lg flex flex-col px-4 py-2 gap-2">
+          <div className="h-[20%] flex justify-between ">
+            {orders && orders.length > 0 ? (
+              orders.map((order) => (
+                <div key={order._id} className="flex flex-col">
+                  <div
+                    key={order._id}
+                    className="text-[#D9D9D9] md:text-sm text-[10px]"
+                  >
+                    <h3>
+                      Order ID: <br />{" "}
+                      <span className="text-primary md:text-lg text-sm">
+                        #{order._id}
+                      </span>
+                    </h3>
+                    <div className="flex flex-row gap-4 mt-4 mb-4">
+                      {order.books.map((book) => (
+                        <div key={book._id} className="flex flex-row">
+                          <div className="lg:w-32 lg:h-48 norm:w-28 norm:h-44 md:w-24 md:h-40 sm:w-20 sm:h-32 w-16 h-28">
+                            <img src={book.imgSrc} alt="" />
+                          </div>
+                          <div className="flex flex-col">
+                            <div className="text-primary sm:text-lg norm:text-2xl lg:text-2xl text-nowrap text-base">
+                              {book.title}
+                            </div>
+                            <p className="text-[#D9D9D9]] sm:text-base norm:text-lg lg:text-lg text-sm">
+                              {book.author}
+                            </p>
+                            <div className="flex-grow"></div>
+                          <button className="ml-2 mb-2 rounded-full bg-secondary sm:px-5 sm:py-2 text-nowrap sm:text-lg p-2 text-[10px] w-[60%]">
+                            View Details
+                          </button>
+                          </div>
+                          
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-              ))}
-              <button className='bg-secondary' onClick={() => { moreDetials(`/orders/${order._id}`) }}>
-                More Details
-              </button>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>No orders found.</p>
-      )
-
-      }
-      <div className='w-[80%] mx-auto'>
-        <h1 className='text-primary text-3xl text-left lg:text-4xl mb-5'>Orders</h1>
-        <div className='border-[1px] border-secondary border-solid rounded-lg flex flex-col px-4 py-2 gap-2'>
-          <div className='h-[20%] flex justify-between '>
-            <div className='flex flex-col'>
-              <h3 className='text-[#D9D9D9] md:text-sm text-[10px]'>Order ID</h3>
-              <h2 className='text-primary md:text-lg text-sm'>#234132</h2>
-            </div>
-            <div className='flex flex-col items-end'>
-              <h2 className='text-[#D9D9D9] md:text-base text-sm text-wrap xs:text-[10px] '>Delivery Date: <span className='text-primary hidden sm:block text-lg'>30th Oct 2024</span></h2>
-              <span className='text-primary block sm:hidden text-base xs:text-sm '>30th Oct 2024</span>
-            </div>
-          </div>
-          <div className='h-[80%] flex justify-between items-end'>
-            <div className='w-[80%]'>
-              <div className='flex gap-4'>
-                <div className="lg:w-32 lg:h-48 norm:w-28 norm:h-44 md:w-24 md:h-40 sm:w-20 sm:h-32 w-16 h-28  bg-white">
-                  Books
-                </div>
-                <div className='w-50% pt-2'>
-                  <h2 className='text-primary sm:text-lg norm:text-2xl lg:text-2xl text-nowrap text-base'>One Piece</h2>
-                  <p className='text-[#D9D9D9]] sm:text-base norm:text-lg lg:text-lg text-sm'> Eiichiro Oda</p>
-                  <p className='text-[#D9D9D9]] sm:text-sm norm:text-base lg:text-base text-[10px]'> Manga</p>
-                </div>
-              </div>
-
-            </div>
-            <div className='w-[20%] flex justify-end'>
-              <button className='rounded-full bg-secondary sm:px-5 sm:py-2 text-nowrap sm:text-lg p-2 text-[10px]'>
-                View Details
-              </button>
-            </div>
+              ))
+            ) : (
+              <p>No orders found.</p>
+            )}
           </div>
         </div>
       </div>
@@ -110,7 +103,7 @@ function orderId() {
         setCurrentPage={setCurrentPage}
       />
     </div>
-  )
+  );
 }
 
-export default orderId
+export default orderId;
