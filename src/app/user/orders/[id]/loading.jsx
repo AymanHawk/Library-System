6322b5dc11347a52abfac87cd104203a0013b433
Katie Andor <superkga5678@gmail.com>
2024-12-13 23:49:32 -0500

    added responsiveness

--- src/app/user/orders/[id]/loading.jsx
+++ src/app/user/orders/[id]/loading.jsx
@@ -5,17 +5,17 @@ function Loading() {
         Array.from({ length: 2 }).map((_, orderIndex) => (
             <div
                 key={orderIndex}
-                className="border-[1px] border-secondary border-solid rounded-lg flex flex-col px-4 mb-2 py-2 gap-2 overflow-x-auto no-scrollbar animate-pulse"
+                className="border-[1px] w-[99%] border-secondary border-solid rounded-lg flex flex-col px-4 mb-2 py-2 gap-2 animate-pulse"
             >
-                <div className="h-[20%] flex justify-between">
-                    <div className="flex flex-col">
-                        <div className="text-loading md:text-sm text-[10px] m-4">
+                <div className="h-[20%] flex justify-between w-[99%]">
+                    <div className="flex flex-col w-[99%]">
+                        <div className="text-loading md:text-sm text-[10px] mt-4 mr-4 ml-4">
                             <div className="bg-loading h-4 w-32 mb-2"></div>
                             <div className="flex flex-row gap-4 mt-4 mb-4">
                                 {Array.from({ length: 3 }).map((_, bookIndex) => (
                                     <div
                                         key={bookIndex}
-                                        className="flex flex-row transition-transform w-[120px] h-[140px] duration-300 hover:scale-[1.01] bg-loading p-2"
+                                        className="flex flex-row lg:w-40 lg:h-48  md:w-40 md:h-48 sm:w-40 sm:h-48 w-[150px] mr-2 duration-300 hover:scale-[1.01] bg-loading p-2"
                                     >
                                     </div>
                                 ))}
--- src/app/user/orders/[id]/page.jsx
+++ src/app/user/orders/[id]/page.jsx
@@ -52,8 +52,8 @@ function orderId() {
     <div>
       <UserNavbar userId={id} userPath={pathname} />
 
-      <div className="w-[80%] mx-[280px]">
-        <h1 className="text-primary text-3xl text-left lg:text-4xl mb-5">
+      <div className="mx-[13px] sm:m-[20px] md:mx-[80px] xl:mx-[80px]">
+        <h1 className="text-primary text-xl sm:text-3xl text-left lg:text-4xl mb-5">
           Orders
         </h1>
         {orders ? (
@@ -64,12 +64,12 @@ function orderId() {
                   <div className="h-[20%] flex justify-between w-[99%]">
                     <div className="flex flex-col w-[99%]">
                       <div
-                        className="text-[#D9D9D9] md:text-sm text-[10px] m-4"
+                        className="text-[#D9D9D9] md:text-sm text-[10px] mt-4 mr-4 ml-4"
                       >
-                        <div className="flex justify-between items-center">
+                        <div className="flex flex-wrap justify-between items-center text-xl">
                           <h3>
                             Order ID: <br />{" "}
-                            <span className="text-primary md:text-lg text-sm">
+                            <span className="text-primary text-xl">
                               #{order._id}
                             </span>
                           </h3>
@@ -83,17 +83,17 @@ function orderId() {
                         </div>
 
 
-                        <div className="flex flex-row gap-4 mt-4 mb-4 p-0.5 overflow-x-auto no-scrollbar">
+                        <div className="flex flex-row gap-4 mt-4 mb-4 p-0.5 overflow-x-auto no-scrollbar h-[260px] mb-2">
                           {order.books.map((book) => (
                             <div key={book._id} className="flex flex-row transition-transform duration-300 hover:scale-[1.01] hover:bg-loading p-2">
-                              <div className="lg:w-32 lg:h-48 norm:w-28 norm:h-44 md:w-24 md:h-40 sm:w-20 sm:h-32 w-16 h-28">
+                              <div className="lg:w-40 lg:h-48  md:w-40 md:h-48 sm:w-40 sm:h-48 w-[150px] mr-2">
                                 <img src={book.imgSrc} alt="" />
                               </div>
                               <div className="flex flex-col">
-                                <div className="text-primary sm:text-lg norm:text-2xl cursor-pointer lg:text-2xl text-nowrap text-base" onClick={() => { handleRoute(`/books/${book._id}`) }}>
+                                <div className="text-primary sm:text-lg md:text-2xl norm:text-2xl cursor-pointer lg:text-2xl text-2xl" onClick={() => { handleRoute(`/books/${book._id}`) }}>
                                   {book.title}
                                 </div>
-                                <p className="text-[#D9D9D9]] sm:text-base norm:text-lg lg:text-lg text-sm">
+                                <p className="text-[#D9D9D9]] sm:text-base md:text-lg norm:text-lg lg:text-lg text-xl">
                                   {book.author}
                                 </p>
                                 <div className="flex-grow"></div>
@@ -105,7 +105,7 @@ function orderId() {
                       </div>
                     </div>
                   </div>
-                  <button onClick={() => { handleRoute(`/orders/${order._id}`) }} className="ml-2 mb-2 rounded-full bg-secondary sm:px-5 sm:py-2 text-nowrap sm:text-lg p-2 text-[10px] w-[25%] transition-transform duration-300 hover:scale-[1.01]">
+                  <button onClick={() => { handleRoute(`/orders/${order._id}`) }} className="ml-2 mb-2 rounded-full bg-secondary sm:px-5 sm:py-2 text-nowrap text-lg p-2 w-[130px] md:w-[180px] transition-transform duration-300 hover:scale-[1.01]">
                     View Details
                   </button>
                 </div>
