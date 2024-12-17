55be27115fa99d6b6bd2bdfa196d251c182396b8
Katie Andor <superkga5678@gmail.com>
2024-12-17 12:00:27 -0500

    fixed responsiveness of recommendation page

--- src/app/user/recommendation/[id]/page.jsx
+++ src/app/user/recommendation/[id]/page.jsx
@@ -57,10 +57,10 @@ export default function Recommendations() {
   return (
     <div>
       <UserNavbar userId={id} userPath={pathname} />
-      <div className="mx-auto 2xl:w-[1400px] xl:w-[1250px] lg:w-[1000px] norm:w-[750px] md:w-[600px] sm:w-[450px] w-[350px] xs:w-[250px]">
-        <div className="flex justify-between items-center">
-          <h1 className="mb-[10px] text-primary text-[43px]">Recommendations</h1>
-          <h1 onClick={() => { handleRoute(`/user/recommendation/survey/${id}`) }} className="mb-[10px] cursor-pointer transition-transform duration-300 hover:scale-[1.01] text-primary text-[25px]">Get New Recomendation</h1>
+      <div className="mx-auto 2xl:w-[1400px] xl:w-[1250px] lg:w-[1000px] norm:w-[750px] md:w-[600px] sm:w-[450px] w-[330px] xs:w-[250px]">
+        <div className="flex flex-wrap justify-between items-center">
+          <h1 className="mb-[10px] text-primary text-[18px] sm:text-[28px] md:text-[43px]">Recommendations</h1>
+          <h1 onClick={() => { handleRoute(`/user/recommendation/survey/${id}`) }} className="mb-[10px] cursor-pointer transition-transform duration-300 hover:scale-[1.01] text-primary text-[15px] sm:text-[18px] md:text-[25px]">Get New Recomendations</h1>
         </div>
         {!list ? (
           <div>
@@ -74,14 +74,14 @@ export default function Recommendations() {
                   {list.map((book) => (
                     <div
                       key={book.id}
-                      className="transition-transform duration-300 hover:scale-[1.01] hover:bg-loading flex flex-col 2xl:w-[256px] w-[250px] h-[550px] xl:w-[226px] xl:h-[600px] lg:w-[220px] norm:w-[210px] lg:h-[630px] md:w-[250px] md:h-[630px] sm:w-[200px] sm:h-[580px]"
+                      className="transition-transform duration-300 hover:scale-[1.01] hover:bg-loading flex flex-col 2xl:w-[256px] w-[250px] h-auto xl:w-[226px] xl:h-[600px] lg:w-[220px] norm:w-[210px] lg:h-[630px] md:w-[250px] md:h-[630px] sm:w-[200px] sm:h-[580px]"
                     >
                       <img
                         src={book.imgUrl}
                         alt={book.id}
                         width={50}
                         height={60}
-                        className="w-full xl:h-[350px] lg:h-[350px] md:h-[350px] h-[300px]"
+                        className="w-full xl:h-[350px] lg:h-[350px] md:h-[350px] h-[350px]"
                       />
                       <h2 onClick={() => handleBookClick(`/books/${book.id}`)} className="text-primary cursor-pointer text-[32px]">
                         {book.title.length > 40
